70b72ce256b7535c605f856cc968e8991175440f
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2024-05-19 06:34:57 +0300

    feat: Implement a method named get_page that takes two integer arguments page with default value 1 and page_size with default value 10

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+"""
+This module contains functions that performs simple pagination on a CSV dataset
+"""
+
+import csv
+import math
+from typing import Tuple, List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Gets The given page content from a CSV dataset
+
+        Args:
+            page: the page number
+            page_size: the number of elements in the page
+
+        Returns:
+            The given page content
+        """
+        assert isinstance(page, int) and page > 0
+
+        assert isinstance(page_size, int) and page_size > 0
+
+        total_size = len(self.dataset())
+        total_pages = (total_size + page_size - 1) // page_size
+        page_range = index_range(page, page_size)
+        if (page_range[0] > total_size - 1) or (page > total_pages):
+            return []
+        else:
+            return self.__dataset[page_range[0]:page_range[1]]
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Calculates the start index and end index corresponding to
+    the range of indexes
+
+    Args:
+        page: the page number
+        page_size: the number of elements in the page
+
+    Returns:
+        A tuple of two values (start_index, end_index)
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
--- 0x00-pagination/README.md
+++ 0x00-pagination/README.md
@@ -10,3 +10,13 @@ The dataset is a CSV file that contains the following columns:
 - `Child's First Name`
 - `Count`
 - `Rank`
+
+## Technologies & Tools :wrench:
+- Python 3.7 or higher
+- CSV module
+- Typing module
+
+
+## Files :card_file_box:
+- `0-simple_helper_function.py`: Contains a function named `index_range` that takes two integer arguments `page` and `page_size` and returns a tuple of size two containing a start index and an end index corresponding to the range of indexes to return in a list for those particular pagination parameters.
+- `1-simple_pagination.py`: Contains a function named `get_page` that takes two integer arguments `page` (default=1) and `page_size` (default=10) and returns a list of the correct page of the dataset.
