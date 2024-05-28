17d97209959a0558143e3d7f01c211e369a00cba
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2024-05-29 00:36:13 +0300

    feat: Get locale from request

--- 0x02-i18n/models/config.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""Module for basic flask app configuration"""
-
-
-class Config:
-    """Config class for Flask app"""
-    LANGUAGES = ["en", "fr"]
-    DEFAULT_LOCALE = "en"
-    TIMEZONE = "UTC"
